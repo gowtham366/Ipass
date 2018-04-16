a8b5964edec138ce26849753d153f82b9a490027
Gowtham C <gowtham366.vpi@gmail.com>
2018-04-16 21:31:42 +0530

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,27 +2,27 @@ var Router = require('express').Router();
 var Api = require('../models/api_collection');
 
 var gresult;
-Router.use('/',function(req, res, next){
-  console.log(req.body);
-  Api.findOne({_id: req.body.api}, function(error, result){
-    if (error) {
-      res.status(500).json({
-          status: false,
-          message: "Error!!! Invalid API Key"
-      });
-    }
-    else if (result.publicRprivate) {
-      res.status(500).json({
-          status: false,
-          message: "Not a Private API Key"
-      });
-    }
-    else {
-      gresult = result;
-      next();
-    }
-  });
-});
+// Router.use('/',function(req, res, next){
+//   console.log(req.body);
+//   Api.findOne({_id: req.body.api}, function(error, result){
+//     if (error) {
+//       res.status(500).json({
+//           status: false,
+//           message: "Error!!! Invalid API Key"
+//       });
+//     }
+//     else if (result.publicRprivate) {
+//       res.status(500).json({
+//           status: false,
+//           message: "Not a Private API Key"
+//       });
+//     }
+//     else {
+//       gresult = result;
+//       next();
+//     }
+//   });
+// });
 
 Router.post('/display',function(req, res, next){
   console.log(req.body);
